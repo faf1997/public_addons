6d63e5de60af280b436911a0d8958d4bbf82ca07
faf1997 <alejandrofranfio@gmail.com>
2024-11-05 11:17:10 -0300

    [UPD]

--- fix_mail_marketing/models/mailing_mailing.py
+++ fix_mail_marketing/models/mailing_mailing.py
@@ -1,30 +1,47 @@
-from odoo import models, fields, api
+from odoo import api, fields, models, tools, _
+from odoo.exceptions import UserError
+import logging
+
 
 class MailingMailing(models.Model):
     _inherit = 'mailing.mailing'
 
     def action_launch(self):
         self.write({'schedule_type': 'now'})
-        return self.action_send_mails()
+        return self.action_put_in_queue()
 
-    def action_send_mails(self):
+    def action_put_in_queue(self):
+        self.write({'state': 'in_queue'})
+        cron = self.env.ref('mass_mailing.ir_cron_mass_mailing_queue')
+        schedule_dates = self.mapped('schedule_date') or [fields.Datetime.now()]
+        for schedule_date in schedule_dates:
+            cron._trigger(schedule_date or fields.Datetime.now())
+
+    def action_send_mail(self):
         for mailing in self:
-            mailing.write({'state': 'sending'})
-            mailing._send_mails()
-            mailing.write({'state': 'done'})
-        return True
+            if not mailing.body_html or mailing.is_body_empty:
+                raise UserError(_('El cuerpo del correo está vacío. Por favor, agrega contenido antes de enviar.'))
 
-    def _send_mails(self):
-        MailMail = self.env['mail.mail']
-        for mailing in self:
-            mailing.ensure_one()
-            mailing_model = self.env[mailing.mailing_model_real]
-            records = mailing._get_records()
-            email_template = mailing._get_email_template()
+            # Obtener los destinatarios
+            res_ids = mailing._get_recipients()
+            if not res_ids:
+                raise UserError(_('No hay destinatarios seleccionados.'))
 
-            for record in records:
-                # Generar el correo electrónico basado en la plantilla y el registro
-                values = email_template.generate_email(record.id)
-                # Crear y enviar el correo
-                mail = MailMail.create(values)
-                mail.send()
+            # Preparar y enviar los correos
+            composer_values = {
+                'subject': mailing.subject,
+                'body': mailing.body_html,
+                'email_from': mailing.email_from,
+                'attachment_ids': [(4, attachment.id) for attachment in mailing.attachment_ids],
+                'reply_to': mailing.reply_to,
+                'mail_server_id': mailing.mail_server_id.id,
+                'mass_mailing_id': mailing.id,
+            }
+
+            composer = self.env['mail.compose.message'].with_context(
+                active_model=mailing.mailing_model_real,
+                active_ids=res_ids
+            ).create(composer_values)
+
+            composer.send_mail()
+            mailing.write({'state': 'done', 'sent_date': fields.Datetime.now()})
